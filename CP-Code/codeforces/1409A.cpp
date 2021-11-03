ae20c14ac363d923640e40d259c51a105c04ec15
PhucCNH <93458094+PhucCNH@users.noreply.github.com>
2021-11-03 23:08:24 +0700

    Create 1409A.cpp

---
+++ CP-Code/codeforces/1409A.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<iomanip>
+#include<algorithm>
+#include<math.h>
+#include<cstring>
+#include<vector>
+#include<utility>
+#include<set>
+#include<queue>
+
+using namespace std;
+
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<long long> bigNum;
+typedef  vector<vector<int>> vii;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long 
+#define fi first
+#define se second 
+#define pb push_back
+#define EL "\n"
+#define FOR(i,a,b) for (int i = a;i<=b;i++) 
+#define DEC(i,a,b) for (int i = a;i>=b;i--)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a/gcd(a,b))*b
+#define debug "#:            "
+
+const int inf = 1e9 + 7;
+const double eps = 1e-6;
+const double pi = 1.00 * acos(-1.00);
+const int NMAX = 1001;
+
+set<int> a;
+
+int main() {
+	fastio
+	int t;
+	ll a,b;
+	cin >> t;
+	while(t--) {
+		cin >> a >> b;
+		ll tmp = max(a,b)-min(a,b);
+		if (tmp %10 == 0) cout << tmp/10 << EL;
+		else cout << tmp/10+1 << EL;
+	}
+	return 0;
+}
+// Code cua Phuc ;-;
