60f8d4b0d0f69bef484fffa57d3b5496508c5c54
PhucCNH <93458094+PhucCNH@users.noreply.github.com>
2021-11-09 10:29:16 +0700

    Create A.cpp

---
+++ CP-Code/Contest/91121/A.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<iomanip>
+#include<algorithm>
+#include<math.h>
+#include<cstring>
+#include<vector>
+#include<utility>
+#include<set>
+#include<queue>
+#include <chrono>
+
+using namespace std;
+
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<long long> bigNum;
+typedef  vector<vector<int>> vii;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long 
+#define fi first
+#define se second 
+#define pb push_back
+#define EL "\n"
+#define FOR(i,a,b) for (int i = a;i<=b;i++) 
+#define DEC(i,a,b) for (int i = a;i>=b;i--)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a/gcd(a,b))*b
+#define time_start() auto start = chrono::steady_clock::now(); 
+#define time_end() auto end = chrono::steady_clock::now();
+#define time_use() cout << EL << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms" << endl;
+
+const int inf = 1e9 + 7;
+const double eps = 1e-6;
+const double pi = 1.00 * acos(-1.00);
+
+int main() {
+	fastio
+	ll l, r;
+    cin >> l >> r;
+    if (r % 2 == 0) r--;
+    if (l % 2 == 0) l++;
+    ll ssh =(r - l)/2+1;
+    ll res =(l + r)*ssh / 2;
+    cout << res;
+	return 0;
+}
+// Code cua Phuc ;-;
