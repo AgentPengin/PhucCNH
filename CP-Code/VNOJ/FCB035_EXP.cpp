6ed17ca7d88250f51da40e2d7889deaa1340893e
PhucCNH <93458094+PhucCNH@users.noreply.github.com>
2022-01-22 00:25:05 +0700

    Create FCB035_EXP.cpp

---
+++ CP-Code/VNOJ/FCB035_EXP.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie();
+#define fi first
+#define se second
+#define ll long long
+#define EL "\n";
+#define time cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n "
+#define db(x) do { std::cerr << #x << ": " << x << EL;} while (0);
+#define int long long
+
+const int inf = 1e9 + 7;
+const double eps = 1e-6;
+const double pi = 1.00 * acos(-1.00);
+
+using namespace std;
+
+signed main() {
+	fastio
+	int test;
+	cin >> test;
+	int a,b,c;
+	while(test--) {
+		cin >> a >> b >> c;
+		int res = inf;
+		res = min(res,abs(a + b - c));
+		res = min(res,abs(a + b * c));
+		res = min(res,abs(a - b * c));
+		res = min(res,abs(a - b + c));
+		res = min(res,abs(a * b - c));
+		res = min(res,abs(a * b + c));
+		res = min(res,abs(a*b*c));
+		res = min(res,abs(a-b-c));
+		res = min(res,abs(a+b+c));
+		cout << res << EL;
+	}
+
+	return 0;
+}
+// Code cua Phuc ༼ つ ◕_◕ ༽つ
