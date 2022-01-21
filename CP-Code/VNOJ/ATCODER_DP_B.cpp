ce30c70792e889f797b60f6d09f96963e3786cef
PhucCNH <93458094+PhucCNH@users.noreply.github.com>
2022-01-21 21:04:48 +0700

    Create ATCODER_DP_B.cpp

---
+++ CP-Code/VNOJ/ATCODER_DP_B.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<iomanip>
+#include<algorithm>
+#include<math.h>
+#include<cstring>
+#include<vector>
+#include<utility>
+#include<set>
+#include<queue>
+#include <chrono>
+#include<map>
+
+using namespace std;
+
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<long long> bigNum;
+typedef  vector<vector<int>> vii;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long 
+#define fi first
+#define se second 
+#define pb push_back
+#define EL "\n"
+#define FOR(i,a,b) for (int i = a;i<=b;i++) 
+#define DEC(i,a,b) for (int i = a;i>=b;i--)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a/gcd(a,b))*b
+#define time_start() auto start = chrono::steady_clock::now(); 
+#define time_end() auto end = chrono::steady_clock::now();
+#define time_use() cout << EL << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms" << endl;
+
+const int inf = 1e9 + 7;
+const double eps = 1e-6;
+const double pi = 1.00 * acos(-1.00);
+const int NMAX = 1e5 + 10;
+
+ll a[NMAX];
+ll f[NMAX];
+
+void file() {
+    freopen("input.inp","r",stdin);
+    freopen("output.out","w",stdout);
+}
+
+int main() {
+    fastio
+    //file();
+    memset(f,inf,sizeof(f));
+    int n,k;
+    cin >> n >> k;
+    FOR(i,0,n-1) cin >> a[i];
+    f[0] = 0;
+    FOR(i,0,n-1) {
+        FOR(j,i + 1,i+k) {
+            if (j < n) f[j] = min(f[j],f[i] + abs(a[i] - a[j])); 
+        }
+    }
+    cout << f[n - 1];
+    return 0;
+}
+// Code cua Phuc ;-;
