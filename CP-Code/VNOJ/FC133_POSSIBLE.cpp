94df68ca891a59907eb8d523d0b60e59e7892a08
PhucCNH <93458094+PhucCNH@users.noreply.github.com>
2022-03-01 23:56:40 +0700

    Create FC133_POSSIBLE.cpp

---
+++ CP-Code/VNOJ/FC133_POSSIBLE.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie();
+#define fi first
+#define se second
+#define ll long long
+#define EL "\n";
+#define time cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n "
+#define db(x) do { std::cerr << #x << ": " << x << EL;} while (0)
+#define int long long
+
+const int inf = 1e9 + 7;
+const double eps = 1e-6;
+const double pi = 1.00 * acos(-1.00);
+
+using namespace std;
+
+signed main() {
+	fastio
+	int n,sum = 0;
+	cin >> n;
+	vector<int> a;
+	for (int i = 0;i<n;i++) {
+		int x;
+		cin >> x;
+		a.push_back(x);
+		sum += x;
+	}
+	sort(a.begin(),a.end());
+	if (a[n-1] > sum) cout << "NO",exit(0);
+	else {
+		if (sum %2 != 0) cout << "NO";
+		else cout << "YES";
+	}
+	
+	
+
+	return 0;
+}
+// Code cua Phuc ༼ つ ◕_◕ ༽つ
