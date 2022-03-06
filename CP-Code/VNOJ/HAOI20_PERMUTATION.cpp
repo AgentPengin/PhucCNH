45ea769962a4be3429f435c04a77f427953581c3
PhucCNH <93458094+PhucCNH@users.noreply.github.com>
2022-03-06 22:10:34 +0700

    Create HAOI20_PERMUTATION.cpp

---
+++ CP-Code/VNOJ/HAOI20_PERMUTATION.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie();
+#define fi first
+#define se second
+#define ll long long
+#define EL "\n";
+#define time cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n "
+#define db(x) do { std::cerr << #x << ": " << x << EL;} while (0)
+#define int long long
+
+const int inf = 1e9 + 7;
+const double eps = 1e-6;
+const double pi = 1.00 * acos(-1.00);
+
+using namespace std;
+
+signed main() {
+	fastio
+	string s;
+	cin >> s;
+	int sum = 0;
+	bool check = false;
+	for (int i = 0;i<s.size();i++) {
+		sum += s[i] - '0';
+		if (s[i] == '0') check = true; 
+	} 
+	if (check == false || sum %3 != 0) return cout << -1,0;
+	sort(s.begin(),s.end());
+	reverse(s.begin(),s.end());
+	cout << s;
+
+	return 0;
+}
+// Code cua Phuc ༼ つ ◕_◕ ༽つ
