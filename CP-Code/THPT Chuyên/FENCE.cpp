5efb2e7284492b223a5c99cd5ae4aa3a56891dd9
PhucCNH <93458094+PhucCNH@users.noreply.github.com>
2022-04-19 15:31:59 +0700

    Create FENCE.cpp

---
+++ CP-Code/THPT Chuyên/FENCE.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie();
+#define fi first
+#define se second
+#define ll long long
+#define EL "\n";
+#define time cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n "
+#define db(x) do { std::cerr << #x << ": " << x << EL;} while (0)
+
+const int inf = 1e9 + 7;
+const double eps = 1e-6;
+const double pi = 1.00 * acos(-1.00);
+const int mod = 100000007;
+
+using namespace std;
+
+ll powerMod(ll n,ll k) {
+	if (k == 0) return 1;
+	if (k == 1) return n;
+	ll tmp = powerMod(n,k/2) % mod;
+	if (k %2 == 0) return (tmp * tmp) % mod;
+	else return ((tmp * tmp) % mod * n % mod) % mod;
+}
+
+signed main() {
+	fastio
+	ll n;
+	cin >> n;
+	n--;
+	cout << powerMod(2,n);
+	
+	return 0;
+}
+// Code cua Phuc ༼ つ ◕_◕ ༽つ
+    
