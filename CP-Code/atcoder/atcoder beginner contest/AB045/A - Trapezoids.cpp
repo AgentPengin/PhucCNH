932a40493c1929a05bd657486cee73a890d85dc4
PhucCNH <93458094+PhucCNH@users.noreply.github.com>
2022-04-27 23:52:45 +0700

    Create A - Trapezoids.cpp

---
+++ CP-Code/atcoder/atcoder beginner contest/AB045/A - Trapezoids.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie();
+#define fi first
+#define se second
+#define ll long long
+#define EL "\n"
+#define time cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n "
+#define db(val) "["#val" = "<<(val)<<"] "
+
+const int inf = 1e9 + 7;
+const double eps = 1e-6;
+const double pi = 1.00 * acos(-1.00);
+
+using namespace std;
+
+ll a,b,h;
+
+signed main() {
+	fastio
+	cin >> a >> b >> h;
+	ll area = (a + b) *h /2;
+	cout << area;
+	
+	
+	return 0;
+}
+// Code cua Phuc ༼ つ ◕_◕ ༽つ
