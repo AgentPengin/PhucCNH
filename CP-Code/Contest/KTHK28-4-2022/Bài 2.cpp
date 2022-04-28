82bacd07e090b21971fe1fbc681e9a4f5ac7ad43
PhucCNH <93458094+PhucCNH@users.noreply.github.com>
2022-04-28 14:16:54 +0700

    Create Bài 2.cpp

---
+++ CP-Code/Contest/KTHK28-4-2022/Bài 2.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie();
+#define fi first
+#define se second
+#define ll long long
+#define EL "\n"
+#define time cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n "
+#define db(val) "["#val" = "<<(val)<<"] "
+
+const int inf = 1e9 + 7;
+const double eps = 1e-6;
+const double pi = 1.00 * acos(-1.00);
+
+using namespace std;
+
+ll x;
+
+signed main() {
+	fastio
+	cin >> x;
+	if (x % 2 == 0) cout << "YES";
+	else cout << "NO";
+	return 0;
+}
+// Code cua Phuc ༼ つ ◕_◕ ༽つ
