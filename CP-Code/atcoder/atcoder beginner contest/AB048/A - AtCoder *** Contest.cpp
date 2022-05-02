5964827b7e7e679fc05c69c0998f5906e0908dfc
PhucCNH <93458094+PhucCNH@users.noreply.github.com>
2022-05-02 19:22:09 +0700

    Create A - AtCoder *** Contest.cpp

---
+++ CP-Code/atcoder/atcoder beginner contest/AB048/A - AtCoder *** Contest.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie();
+#define fi first
+#define se second
+#define ll long long
+#define EL "\n"
+#define time cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n "
+#define db(val) "["#val" = "<<(val)<<"] "
+
+const int inf = 1e9 + 7;
+const double eps = 1e-6;
+const double pi = 1.00 * acos(-1.00);
+
+using namespace std;
+
+signed main() {
+	fastio
+	string s;
+	getline(cin,s);
+	cout << s[0];
+	for (int i = 1;i<s.size();i++) {
+		if (s[i-1] == ' ') cout << s[i];
+	}
+	return 0;
+}
+// Code cua Phuc ༼ つ ◕_◕ ༽つ
