afae4b15082290e8f8b0a560ae23d3eeaa3a533f
PhucCNH <93458094+PhucCNH@users.noreply.github.com>
2022-05-02 21:22:13 +0700

    Create D - An Ordinary Game.cpp

---
+++ CP-Code/atcoder/atcoder beginner contest/AB048/D - An Ordinary Game.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie();
+#define fi first
+#define se second
+#define ll long long
+#define EL "\n"
+#define time cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n "
+#define db(val) "["#val" = "<<(val)<<"] "
+#define int long long
+
+const int inf = 1e9 + 7;
+const double eps = 1e-6;
+const double pi = 1.00 * acos(-1.00);
+
+using namespace std;
+
+signed main() {
+	fastio
+	string s;
+	getline(cin,s);
+	int len = s.size();
+	if (s[0] == s[len - 1]) len--;
+	if (len % 2 == 0) cout << "Second";
+	else cout << "First";
+
+	
+	return 0;
+}
+// Code cua Phuc ༼ つ ◕_◕ ༽つ
