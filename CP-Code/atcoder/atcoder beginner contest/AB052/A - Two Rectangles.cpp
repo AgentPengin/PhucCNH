12464a96b28f3cfb09a5017de4d546689d473873
PhucCNH <93458094+PhucCNH@users.noreply.github.com>
2022-05-04 23:00:44 +0700

    Create A - Two Rectangles.cpp

---
+++ CP-Code/atcoder/atcoder beginner contest/AB052/A - Two Rectangles.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie();
+#define fi first
+#define se second
+#define ll long long
+#define EL "\n"
+#define time cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n "
+#define db(val) "["#val" = "<<(val)<<"] "
+
+const int inf = 1e9 + 7;
+const double eps = 1e-6;
+const double pi = 1.00 * acos(-1.00);
+
+using namespace std;
+
+signed main() {
+	fastio
+	ll a,b,c,d;
+	cin >> a >> b >> c >> d;
+	cout << max(a*b,c*d);
+
+
+	return 0;
+}
+// Code cua Phuc ༼ つ ◕_◕ ༽つ
