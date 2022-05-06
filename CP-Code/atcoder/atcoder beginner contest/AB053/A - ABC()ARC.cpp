22243045fa872073e182355970e851c8a7049277
PhucCNH <93458094+PhucCNH@users.noreply.github.com>
2022-05-06 23:13:01 +0700

    Create A - ABC()ARC.cpp

---
+++ CP-Code/atcoder/atcoder beginner contest/AB053/A - ABC()ARC.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie();
+#define fi first
+#define se second
+#define ll long long
+#define EL "\n"
+#define time cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n "
+#define db(val) "["#val" = "<<(val)<<"] "
+
+const int inf = 1e9 + 7;
+const double eps = 1e-6;
+const double pi = 1.00 * acos(-1.00);
+
+using namespace std;
+
+signed main() {
+	fastio
+	int x;
+	cin >> x;
+	if (x < 1200) cout << "ABC";
+	else cout << "ARC";
+	
+	
+	return 0;
+}
+// Code cua Phuc ༼ つ ◕_◕ ༽つ
